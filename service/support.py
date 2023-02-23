6a5fac14e7121802313acae2c66b290db0ea2c9a
burasate <61697687+burasate@users.noreply.github.com>
2023-02-24 00:17:28 +0700

    Update support.py

--- service/support.py
+++ service/support.py
@@ -13,7 +13,7 @@ import getpass, os
 url = 'https://raw.githubusercontent.com/burasate/keysTweener/main/BRS_KeysTweener.py'
 u_read = uLib.urlopen(url).read()
 u_read = u_read.replace('$usr_orig$', getpass.getuser())
-print(u_read)
+#print(u_read)
 print(script_path)
 #with open(script_path, 'w') as f:
     #f.writelines(u_read)
